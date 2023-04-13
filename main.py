c5aad7bb385401b5857e9ea72a81f85aaaf73407
lzz-129 <3044178018@qq.com>
2023-04-13 22:53:42 +0200

    add regex reference

--- main.py
+++ main.py
@@ -29,6 +29,10 @@ def _genShort(url, size = HASH_LEN):
     return short
 
 def check_url(url):
+    """
+        The following is adapted from other project 
+        github link:https://github.com/django/django/blob/stable/1.3.x/django/core/validators.py#L45
+    """
     regex = re.compile(
         r'^(?:http|ftp)s?://'  # http:// or https://
         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
