e9d58865c76db20ef6d808bbdfc089a4e78c0bcd
vlowingkloude <vlowingkloud@proton.me>
2023-05-10 21:50:37 +0200

    bug fix

--- prepare/create_secrets.sh
+++ prepare/create_secrets.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
-kubectl create secret generic my-secret --from-literal=strongkey
+#kubectl create secret generic my-secret --from-literal=strongkey
 
-kubectl create secret generic my-secret --from-literal=DBADDR=http://db-service/\x20DBUSER=postgres\x20DBPASSWORD=password
+kubectl create secret generic dbconfig --from-literal=DBADDR=http://db-service/\x20DBUSER=postgres\x20DBPASSWORD=password
